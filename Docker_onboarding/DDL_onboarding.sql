16896de9b4b3cd2b1281579fc38e114e012a01d8
NicholasDRR <nicholasreis00@gmail.com>
2023-08-02 17:27:01 -0300

    feat(environment): mudando DDL

--- Docker_onboarding/DDL_onboarding.sql
+++ Docker_onboarding/DDL_onboarding.sql
@@ -1,212 +1,209 @@
-CREATE DATABASE IF NOT EXISTS onboarding_db DEFAULT CHARACTER SET utf8;
-USE onboarding_db;
+CREATE DATABASE IF NOT EXISTS onboarding_me DEFAULT CHARACTER SET utf8;
+USE onboarding_me;
 
--- create a table Enterprise
 
-CREATE TABLE IF NOT EXISTS Enterprise(
-    id_enterprise INT NOT NULL AUTO_INCREMENT,
-    fictitious_name VARCHAR(100) NOT NULL,
-    company_name VARCHAR(100) NOT NULL,
-    state_registration VARCHAR(12) NOT NULL UNIQUE,
-    cnpj VARCHAR(14) NOT NULL UNIQUE,
-    logo BLOB,
-    password_login VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY(id_enterprise)
+CREATE TABLE IF NOT EXISTS Company (
+  id BIGINT AUTO_INCREMENT,
+  company_name VARCHAR(255) NOT NULL,
+  trading_name VARCHAR(255),
+  logo BLOB,
+  cnpj VARCHAR(14) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  company_password VARCHAR(255) NOT NULL,
+  state_register VARCHAR(14),
+  PRIMARY KEY (id)
 );
 
--- create a table Addres
-
-CREATE TABLE IF NOT EXISTS Address(
-    id_address INT NOT NULL AUTO_INCREMENT,
-    cep VARCHAR(9) NOT NULL,
-    state VARCHAR(30) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    street VARCHAR(100) NOT NULL,
-    number INT NOT NULL,
-    neighbourhood VARCHAR(50) NOT NULL,
-    complement VARCHAR(100),
-    PRIMARY KEY(id_address)
+CREATE TABLE IF NOT EXISTS Address (
+  id BIGINT AUTO_INCREMENT,
+  num VARCHAR(255) NOT NULL,
+  complement VARCHAR(255),
+  zipcode VARCHAR(8) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  district VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  state VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
--- create a table Employee
-
-CREATE TABLE IF NOT EXISTS Employee(
-    id_employee INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    phone_number VARCHAR(14) NOT NULL,
-    corporate_email VARCHAR(255) NOT NULL UNIQUE,
-    birth_date DATE NOT NULL,
-    job_position VARCHAR(100) NOT NULL,
-    password_login VARCHAR(50) NOT NULL,
-    PRIMARY KEY(id_employee),
-    id_enterprise INT,
-    id_address INT,
-    FOREIGN KEY(id_enterprise) REFERENCES Enterprise(id_enterprise),
-    FOREIGN KEY(id_address) REFERENCES Address(id_address)
+CREATE TABLE IF NOT EXISTS Employee (
+  id BIGINT AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  birthdate DATE NOT NULL,
+  employee_role VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  employee_password VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(14),
+  cpf VARCHAR(11) NOT NULL UNIQUE,
+  level_access ENUM('admin', 'manager', 'default') NOT NULL,
+  company_id BIGINT NOT NULL,
+  address_id BIGINT NOT NULL,
+  FOREIGN KEY (company_id) REFERENCES Company (id),
+  FOREIGN KEY (address_id) REFERENCES Address (id),
+  PRIMARY KEY (id)
 );
 
--- create a table WelcomeKit
-
-CREATE TABLE IF NOT EXISTS WelcomeKit(
-    id_welcomekit INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    image BLOB NOT NULL,
-    PRIMARY KEY(id_welcomekit)
+CREATE TABLE IF NOT EXISTS WelcomeKit (
+  id BIGINT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  image BLOB,
+  PRIMARY KEY (id)
 );
 
--- create a table WelcomeKit_Item
-
-CREATE TABLE IF NOT EXISTS WelcomeKit_Item(
-    id_welcomekit_item INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    image BLOB NOT NULL,
-    PRIMARY KEY(id_welcomekit_item)
+CREATE TABLE IF NOT EXISTS WelcomeKitItem (
+  id BIGINT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  image BLOB NOT NULL,
+  PRIMARY KEY (id)
 );
 
--- create a table WelcomeKit_Connection
-
-CREATE TABLE IF NOT EXISTS WelcomeKit_Connection(
-    id INT NOT NULL AUTO_INCREMENT,
-    id_welcomekit_item INT,
-    id_welcomekit INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_welcomekit_item) REFERENCES WelcomeKit_Item(id_welcomekit_item),
-    FOREIGN KEY(id_welcomekit) REFERENCES WelcomeKit(id_welcomekit)
+CREATE TABLE IF NOT EXISTS WelcomeKit_WelcomeKitItem (
+  id BIGINT AUTO_INCREMENT,
+  welcome_kit_id BIGINT NOT NULL,
+  item_id BIGINT NOT NULL,
+  FOREIGN KEY (welcome_kit_id) REFERENCES WelcomeKit (id),
+  FOREIGN KEY (item_id) REFERENCES WelcomeKitItem (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Tracking
-
-CREATE TABLE IF NOT EXISTS Tracking(
-    id_tracking INT NOT NULL AUTO_INCREMENT,
-    tracking_num VARCHAR(13) NOT NULL,
-    status_tracking ENUM('in-preparation', 'sended', 'delivered') NOT NULL,
-    id_employee INT UNIQUE,
-    id_welcomekit INT,
-    PRIMARY KEY(id_tracking),
-    FOREIGN KEY(id_employee) REFERENCES Employee(id_employee),
-    FOREIGN KEY(id_welcomekit) REFERENCES WelcomeKit(id_welcomekit)
+CREATE TABLE IF NOT EXISTS Tracking (
+  id BIGINT AUTO_INCREMENT,
+  tracking_code VARCHAR(255) NOT NULL UNIQUE,
+  status ENUM('to_be_send','sended','delivered') NOT NULL,
+  employee_id BIGINT NOT NULL,
+  welcome_kit_id BIGINT NOT NULL,
+  FOREIGN KEY (employee_id) REFERENCES Employee (id),
+  FOREIGN KEY (welcome_kit_id) REFERENCES WelcomeKit (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Gamified_Journey
-
-CREATE TABLE IF NOT EXISTS Gamified_Journey(
-    id_gamified INT NOT NULL AUTO_INCREMENT,
-    welcome_video_link VARCHAR(255) NOT NULL,
-    id_enterprise INT UNIQUE,
-    PRIMARY KEY(id_gamified),
-    FOREIGN KEY(id_enterprise) REFERENCES Enterprise(id_enterprise)
+CREATE TABLE IF NOT EXISTS Chat (
+  id BIGINT AUTO_INCREMENT,
+  company_id BIGINT,
+  first_employee_id BIGINT NOT NULL,
+  FOREIGN KEY (company_id) REFERENCES Company (id),
+  FOREIGN KEY (first_employee_id) REFERENCES Employee (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Game
-
-CREATE TABLE IF NOT EXISTS Game(
-    id_game INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    id_gamified INT,
-    PRIMARY KEY(id_game),
-    FOREIGN KEY(id_gamified) REFERENCES Gamified_Journey(id_gamified)
+CREATE TABLE IF NOT EXISTS Chat_Employee (
+  id BIGINT AUTO_INCREMENT,
+  chat_id BIGINT NOT NULL,
+  second_employee_id BIGINT NOT NULL,
+  FOREIGN KEY (chat_id) REFERENCES Chat (id),
+  FOREIGN KEY (second_employee_id) REFERENCES Employee (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Quiz
-
-CREATE TABLE IF NOT EXISTS Quiz(
-    id_quiz INT NOT NULL AUTO_INCREMENT,
-    link_video VARCHAR(255) NOT NULL,
-    type ENUM('culture', 'principles') NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    question VARCHAR(255) NOT NULL,
-    score INT NOT NULL,
-    game_id INT,
-    PRIMARY KEY(id_quiz),
-    FOREIGN KEY(game_id) REFERENCES Game(id_game)
+CREATE TABLE IF NOT EXISTS Message (
+  id BIGINT AUTO_INCREMENT,
+  user_to BIGINT NOT NULL,
+  user_from BIGINT NOT NULL,
+  message VARCHAR(255) NOT NULL,
+  created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  read_date TIMESTAMP,
+  chat_id BIGINT NOT NULL,
+  FOREIGN KEY (chat_id) REFERENCES Chat (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Alternative
-
-CREATE TABLE IF NOT EXISTS Alternative(
-    id_alternative INT NOT NULL AUTO_INCREMENT,
-    is_answer TINYINT NOT NULL,
-    answer VARCHAR(255) NOT NULL,
-    quiz_id INT,
-    PRIMARY KEY(id_alternative),
-    FOREIGN KEY(quiz_id) REFERENCES Quiz(id_quiz)
+CREATE TABLE IF NOT EXISTS GamifiedJourney (
+  id BIGINT AUTO_INCREMENT,
+  welcome_video_link VARCHAR(255),
+  company_id BIGINT NOT NULL,
+  FOREIGN KEY (company_id) REFERENCES Company (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Alternative_Employee
-
-CREATE TABLE IF NOT EXISTS Alternative_Employee(
-    id INT NOT NULL AUTO_INCREMENT,
-    id_alternative INT,
-    id_employee INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_alternative) REFERENCES Alternative(id_alternative),
-    FOREIGN KEY(id_employee) REFERENCES Employee(id_employee)
+CREATE TABLE IF NOT EXISTS Game (
+  id BIGINT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  gamified_journey_id BIGINT NOT NULL,
+  FOREIGN KEY (gamified_journey_id) REFERENCES GamifiedJourney (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Category_Tool
-
-CREATE TABLE IF NOT EXISTS Category_Tool(
-    id_category_tool INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    PRIMARY KEY(id_category_tool)
+CREATE TABLE IF NOT EXISTS Medal (
+  id BIGINT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  image BLOB NOT NULL,
+  game_id BIGINT NOT NULL,
+  FOREIGN KEY (game_id) REFERENCES Game (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Tool
-
-CREATE TABLE IF NOT EXISTS Tool(
-    id_tool INT NOT NULL AUTO_INCREMENT,
-    name_tool VARCHAR(100) NOT NULL,
-    link_download VARCHAR(255) NOT NULL,
-    score INT NOT NULL,
-    category_tool_id INT,
-    game_id INT,
-    PRIMARY KEY(id_tool),
-    FOREIGN KEY(category_tool_id) REFERENCES Category_Tool(id_category_tool),
-    FOREIGN KEY(game_id) REFERENCES Game(id_game)
+CREATE TABLE IF NOT EXISTS Score (
+  id BIGINT AUTO_INCREMENT,
+  total_points INTEGER NOT NULL DEFAULT 0,
+  last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  employee_id BIGINT NOT NULL,
+  FOREIGN KEY (employee_id) REFERENCES Employee (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Tool_Employee
-
-CREATE TABLE IF NOT EXISTS Tool_Employee(
-    id INT NOT NULL AUTO_INCREMENT,
-    id_tool INT,
-    id_employee INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_tool) REFERENCES Tool(id_tool),
-    FOREIGN KEY(id_employee) REFERENCES Employee(id_employee)
+CREATE TABLE IF NOT EXISTS Medal_Score (
+  id BIGINT AUTO_INCREMENT,
+  medal_id BIGINT NOT NULL,
+  score_id BIGINT NOT NULL,
+  FOREIGN KEY (medal_id) REFERENCES Medal (id),
+  FOREIGN KEY (score_id) REFERENCES Score (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Medals
-
-CREATE TABLE IF NOT EXISTS Medals(
-    id_medals INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    image BLOB NOT NULL,
-    game_id INT,
-    PRIMARY KEY(id_medals),
-    FOREIGN KEY(game_id) REFERENCES Game(id_game)
+CREATE TABLE IF NOT EXISTS Quiz (
+  id BIGINT AUTO_INCREMENT,
+  link_video VARCHAR(255) NOT NULL,
+  score INTEGER NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  question VARCHAR(255) NOT NULL,
+  quiz_type ENUM('culture','principle') NOT NULL,
+  game_id BIGINT NOT NULL,
+  FOREIGN KEY (game_id) REFERENCES Medal (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Score
-
-CREATE TABLE IF NOT EXISTS Score(
-    id_score INT NOT NULL AUTO_INCREMENT,
-    total_points INT NOT NULL,
-    last_updated INT NOT NULL,
-    id_employee INT UNIQUE,
-    PRIMARY KEY(id_score),
-    FOREIGN KEY(id_employee) REFERENCES Employee(id_employee)
+CREATE TABLE IF NOT EXISTS Alternative (
+  id BIGINT AUTO_INCREMENT,
+  alternative_text VARCHAR(255) NOT NULL,
+  is_answer TINYINT NOT NULL DEFAULT 0,
+  quiz_id BIGINT NOT NULL,
+  FOREIGN KEY (quiz_id) REFERENCES Quiz (id),
+  PRIMARY KEY (id)
 );
 
--- create a table Medals_Score
+CREATE TABLE IF NOT EXISTS CategoryTool (
+  id BIGINT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS Medals_Score(
-    id INT NOT NULL AUTO_INCREMENT,
-    id_medals INT,
-    id_score INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_medals) REFERENCES Medals(id_medals),
-    FOREIGN KEY(id_score) REFERENCES Score(id_score)
+CREATE TABLE IF NOT EXISTS Tool (
+  id BIGINT AUTO_INCREMENT,
+  link_download VARCHAR(255) NOT NULL,
+  score INTEGER NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  game_id BIGINT NOT NULL,
+  category_id BIGINT NOT NULL,
+  FOREIGN KEY (game_id) REFERENCES Medal (id),
+  FOREIGN KEY (category_id) REFERENCES CategoryTool (id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Employee_Alternative (
+  id BIGINT AUTO_INCREMENT,
+  employee_id BIGINT NOT NULL,
+  alternative_id BIGINT NOT NULL,
+  FOREIGN KEY (employee_id) REFERENCES Employee (id),
+  FOREIGN KEY (alternative_id) REFERENCES Alternative (id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Employee_Tool (
+  id BIGINT AUTO_INCREMENT,
+  employee_id BIGINT NOT NULL,
+  tool_id BIGINT NOT NULL,
+  FOREIGN KEY (employee_id) REFERENCES Employee (id),
+  FOREIGN KEY (tool_id) REFERENCES Tool (id),
+  PRIMARY KEY (id)
 );
