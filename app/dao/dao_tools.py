5e90dee9f84f9971c2dc77d0b03bd1ca56c884e5
Victor Sousa Lima <victor@pop-os.localdomain>
2023-08-08 21:49:35 -0300

    feat(tools): refatorando o código e resolvendo alguns erros

--- app/dao/dao_tools.py
+++ app/dao/dao_tools.py
@@ -33,17 +33,20 @@ def insert_tool(tool: Tool):
     VALUES
     ('{tool.link_download}', '{tool.name}', {tool.score}, 1, 1);
     """
+    try:
+        cursor.execute(query)
+    except Exception as error:
+        return None
+    else:
+        connection.commit()
 
-    cursor.execute(query)
-    connection.commit()
+        query = f'SELECT name FROM Tool WHERE name = "{tool.name}"'
+        
+        cursor.execute(query)
+        tool_result = cursor.fetchone()
+        connection.close()
 
-    query = f'SELECT name FROM Tool WHERE name = "{tool.name}"'
-
-    cursor.execute(query)
-    tool_result = cursor.fetchone()
-    connection.close()
-
-    return tool_result
+        return tool_result
 
 def update_tool(tool: Tool):
 
@@ -52,19 +55,36 @@ def update_tool(tool: Tool):
     query = f"""
     UPDATE Tool
     SET link_download = '{tool.link_download}', name = '{tool.name}', score = {tool.score} 
-    WHERE id = 3;
+    WHERE id = {tool.id_tool};
     """
+    try:
+        cursor.execute(query)
+    except Exception as error:
+        return False
+    else:
+        connection.commit()
+        connection.close()
 
-    cursor.execute(query)
-    connection.commit()
-
-    query = f'SELECT name FROM Tool WHERE name = "{tool.name}";'
-
-    cursor.execute(query)
-    tool_result = cursor.fetchone()
-    connection.close()
-
-    return tool_result
+        return True
 
 
-#def verify_tool_exists(tool: Tool):
+def verify_tool_exists(name: str = None, id_tool: int = None):
+
+    connection, cursor = connect_database()
+
+    if id_tool:
+        query = f"SELECT id FROM Tool WHERE id = {id_tool}"
+    else:
+        query = f"SELECT id FROM Tool WHERE id = {name}"
+    
+    try:
+        cursor.execute(query)
+    except Exception as error:
+        return False
+    else:
+        tool_id = cursor.fetchone()
+
+        connection.close()
+
+        return bool(tool_id)
+    
--- app/routers/tool.py
+++ app/routers/tool.py
@@ -1,5 +1,5 @@
 from app.schemas.tool import Tool
-from app.dao.dao_tools import select_tool, insert_tool, update_tool
+from app.dao.dao_tools import select_tool, insert_tool, update_tool, verify_tool_exists
 
 from fastapi import APIRouter,status, HTTPException
 from fastapi.responses import JSONResponse
@@ -23,26 +23,36 @@ def get_tool(company_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "This company don't have tools!"})
 
 
-@router.post("/")
+@router.post("/") 
 def register_tool(tool: Tool):
 
-    tool_registered = insert_tool(tool)
+    tool_exists = verify_tool_exists(name=tool.name)
 
-    if tool_registered:
-        return JSONResponse(status_code=status.HTTP_200_OK, content=tool_registered)
+    if tool_exists:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg":"This tool is already registered!"})
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "The tool has not been registered!"})
+        tool_registered = insert_tool(tool)
+
+        if tool_registered:
+            return JSONResponse(status_code=status.HTTP_200_OK, content=tool_registered)
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "The tool has not been registered!"})
 
 
 @router.put("/")
 def modify_tool(tool: Tool):
 
-    if tool.id_tool:
-        tool_registered = update_tool(tool)
+    tool_exists = verify_tool_exists(id_tool=tool.id_tool)
 
-        if tool_registered:
-            return JSONResponse(status_code=status.HTTP_200_OK, content=tool_registered)
+    if tool_exists:
+        if tool.id_tool:
+            tool_registered = update_tool(tool)
+
+            if tool_registered:
+                return JSONResponse(status_code=status.HTTP_200_OK, content={"msg": "Success updated!"})
+            else:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "The tool has not been modified!"})
         else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "The tool has not been modified!"})
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail={"msg": "Missing id tool!"})
     else:
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail={"msg": "Missing id tool!"})
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg":"This tool is not registered!"})
