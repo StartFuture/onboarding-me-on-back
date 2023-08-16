c13c70ef6a92a05c83425f35f22875f9df25b254
NicholasDRR <nicholasreis00@gmail.com>
2023-08-16 01:46:53 -0300

    feat(email): criando funcao send email

--- app/parameters.py
+++ app/parameters.py
@@ -10,6 +10,8 @@ USERNAME = os.getenv("USERNAME_DB")
 PASSWORD = os.getenv("PASSWORD")
 DATABASE = os.getenv("DATABASE")
 PORT = os.getenv("PORT")
+EMAIL = os.getenv("EMAIL")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 NAME_PATTERN = r"^[A-Za-zÀ-ÿ\s\.\-0-9]+$"
 LINK_DOWNLOAD_PATTERN = r"https\:\/\/[www\.]?[A-z\.0-9]+[\.A-z]+\/.*"
@@ -17,3 +19,9 @@ LINK_DOWNLOAD_PATTERN = r"https\:\/\/[www\.]?[A-z\.0-9]+[\.A-z]+\/.*"
 dict_regex = {'name pattern' : r"^[A-Za-zÀ-ÿ\s\.\-0-9]+$",
               'link download pattern' : r"https\:\/\/[www\.]?[A-z\.0-9]+[\.A-z]+\/.*",
               'cnpj pattern': r"^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$"}
+
+
+subject_congratulations_email = f'Quiz'
+
+congratulations_email =  f""" 
+<h1>Parabéns,</h1>Você alcançou mais de 90% da pontuação!!!</h1>"""
--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,30 @@
+import email
+import smtplib
+
+from parameters import EMAIL, EMAIL_PASSWORD, congratulations_email, subject_congratulations_email
+
+
 def string_to_lower(name: str):
     return name.lower()
+
+
+def send_email(employee_email, email_body, email_subject):
+    
+
+    msg = email.message.Message()
+    msg['Subject'] = subject_congratulations_email
+    msg['From'] = EMAIL
+    msg['To'] = employee_email
+    password = EMAIL_PASSWORD
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(email_body)
+
+    if password:
+        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s.starttls()
+        # Login Credentials for sending the mail
+        s.login(msg['From'], password)
+        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+        
+        
+send_email(employee_email= "nicholasreis00@gmail.com", email_body=congratulations_email, email_subject=subject_congratulations_email)
