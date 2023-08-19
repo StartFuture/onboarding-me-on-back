8836a523c93e26e9394c975a78c59e1e6d6795c9
Tereza Sophia <terezasophia1827@gmail.com>
2023-08-19 15:36:49 -0300

    pequenas alterações

--- app/routers/medal.py
+++ app/routers/medal.py
@@ -12,13 +12,13 @@ router = APIRouter(
         "medal"
     ]
     
-                   )
+)
 
 
 @router.get("/get-medal")
 def get_medal(medal_id: int):
     
-    medal_user = dao.select_medal(medal_id=id)
+    medal_user = select_medal(medal_id=id)
     
     if medal_user:
         return JSONResponse(status_code=status.HTTP_200_OK, content = medal_user)
@@ -27,7 +27,7 @@ def get_medal(medal_id: int):
 
 @router.post("/create-medal")
 def create_medal(name: str, medal_id: int):
-    insert_medal_user = dao.insert_medal(medal_id=id)
+    insert_medal_user = insert_medal(medal_id=id)
     
     if insert_medal_user:
         return JSONResponse(status_code=status.HTTP_200_OK, content=insert_medal_user)
@@ -38,9 +38,9 @@ def create_medal(name: str, medal_id: int):
 
 @router.post("/update-medal")
 def modify_medal(name: str, medal_id: int):
-    modify_medal__user = dao.modify_medal(medal_id=id)
+    modify_medal__user = modify_medal(medal_id=id)
     
-    if modify_medal__userr:
+    if modify_medal__user:
         return JSONResponse(status_code=status.HTTP_200_OK, content=modify_medal__user)
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "This user doesn't have a medal!"})
