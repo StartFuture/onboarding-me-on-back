fbfdbb3508cdc5f130fe52ccabfdc0a129b8d02e
Tereza Sophia <terezasophia1827@gmail.com>
2023-08-24 23:12:12 -0300

    alterações

--- app/dao/dao_medal.py
+++ app/dao/dao_medal.py
@@ -5,7 +5,7 @@ def select_medal(medal_id: int):
     connection, cursor = connect_database()
     
     query = f"""
-    SELECT name, image, medal_id from GamifiedJourney
+    SELECT name, image, medal_id from Medal_Score
 	WHERE medal_id = '{medal_id}';
     """
 
@@ -21,7 +21,7 @@ def select_medal(medal_id: int):
 
     return medal
 
-def insert_medal(medal_id: int, score_id):
+def insert_medal(medal_id: int, score_id: int):
     
     connection, cursor = connect_database()
     
@@ -31,10 +31,78 @@ def insert_medal(medal_id: int, score_id):
     
     try:
         cursor.execute(query)
-    except:
-        None
+    except Exception as error:
+        return False
     finally:
         connection.commit()
         connection.close()
 
     return True
+
+
+def select_medal_by_employee(employee_id: int):
+    
+    connection, cursor = connect_database()
+    
+    query = f"""
+    SELECT ms.medal_id from Score s 
+    left join Medal_Score ms on s.id = ms.score_id 
+    WHERE s.employee_id = {employee_id}
+    ;
+    """
+
+    try:
+        cursor.execute(query)
+    except:
+        medal = None
+    else:
+        medal = cursor.fetchone()
+    finally:
+        connection.commit()
+        connection.close()
+
+    return medal
+
+def select_score_by_employee(employee_id: int):
+    
+    connection, cursor = connect_database()
+    
+    query = f"""
+    SELECT id from Score  
+    WHERE employee_id = {employee_id}
+    ;
+    """
+
+    try:
+        cursor.execute(query)
+    except:
+        score_id = None
+    else:
+        score_id = cursor.fetchone()
+    finally:
+        connection.commit()
+        connection.close()
+
+    return score_id
+
+def select_medal_by_game(game_id: int):
+    
+    connection, cursor = connect_database()
+    
+    query = f"""
+    SELECT id from Medal m 
+    WHERE game_id = {game_id}
+    ;
+    """
+
+    try:
+        cursor.execute(query)
+    except:
+        medal_id = None
+    else:
+        medal_id = cursor.fetchone()
+    finally:
+        connection.commit()
+        connection.close()
+
+    return medal_id
--- app/routers/medal_score.py
+++ app/routers/medal_score.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,status, HTTPException
 from fastapi.responses import JSONResponse
 
-from app.dao.dao_medal import insert_medal
+from app.dao.dao_medal import insert_medal, select_medal_by_employee, select_medal_by_game, select_score_by_employee
 from app.schemas.medal_score_employee import Medal_Score_Employee
 from fastapi import APIRouter
 
@@ -13,32 +13,33 @@ router = APIRouter(
     
 )
 
-@router.get("/get/{medal_id}")
-def get_medal(medal_score_employee: Medal_Score_Employee):
+@router.get("/get/{employee_id}")
+def get_medal(employee_id: int):
     
-    if medal_score_employee.employee_id and medal_score_employee.game_id == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "This employee doesn't have a medal!"})
+    medal = select_medal_by_employee(employee_id=employee_id)
+    
+    if medal:
+        return JSONResponse(status_code=status.HTTP_200_OK, content=medal)
 
     else:
-        get_medal_user = get_medal(medal_id=medal_id)
-        return JSONResponse(status_code=status.HTTP_200_OK, content=get_medal_user)
-
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "This employee doesn't have a medal!"})
 
 @router.post("/create")
 def create_medal(medal_score_employee: Medal_Score_Employee):
     
-    if medal_score_employee.employee_id and medal_score_employee.game_id:
-        insert_medal_user = insert_medal(medal_id=medal_id)
-        return JSONResponse(status_code=status.HTTP_200_OK, content=insert_medal_user)
+    medal_id = select_medal_by_game(game_id=medal_score_employee.game_id)
+    
+    if not medal_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "Game_id doesn't exists!"})
+    
+    score_id = select_score_by_employee(employee_id=medal_score_employee.employee_id)
+    
+    if not score_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "employee_id doesn't exists!"})
+    
+    success = insert_medal(medal_id=medal_id["id"], score_id=score_id["id"]) 
+    
+    if success:
+        return JSONResponse(status_code=status.HTTP_200_OK, content=success)
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "Unable to create the medal for the user!"})
-
-@router.put("/update")
-def update_medal(medal_score_employee: Medal_Score_Employee):
-    
-    if medal_score_employee.employee_id and medal_score_employee.game_id:
-        update_medal_user = update_medal(medal_id=medal_id)
-        return JSONResponse(status_code=status.HTTP_200_OK, content={"msg": "The medal has been deleted!"})
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "This medal couldn't be deleted!"})
