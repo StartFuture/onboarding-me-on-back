aa9c7ccafe5ccd6ad07e875a2d7fcea32d6bdcf1
victorsousalima <victorsousa16042005@gmail.com>
2023-09-19 18:54:10 -0300

    refac(return_jwt): tirando um print do track kit

--- app/dao/dao_track_kit.py
+++ app/dao/dao_track_kit.py
@@ -57,8 +57,9 @@ def update_track_status_to_sended(tracking_id: int, tracking_code: str):
     try:
         cursor.execute(query)
     except Exception as error:
-        print(error)
+        connection.close()
         return False
+    
     finally:
         connection.commit()
         connection.close()
